2d7c08fe657ee3556fbe7d54d761f8f290e07412
Yair Nunez <yair.nunez@accenture.com>
2019-04-04 17:53:55 -0600

    Adding view post page

--- src/app/pages/post-detail/post-detail.component.ts
+++ src/app/pages/post-detail/post-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../../common/models/post';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'src/app/common/services/post.service';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-detail',
@@ -10,10 +11,14 @@ import { PostService } from 'src/app/common/services/post.service';
 })
 export class PostDetailComponent implements OnInit {
   post: Post;
+  form: FormGroup;
 
   constructor(private postService: PostService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      comment: new FormControl()
+   })
     this.getPost();
   }
 
